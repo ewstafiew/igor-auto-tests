3cab835a556fef7bc1c5a58287ccb2b39fc8161a
Igor Ewstafiew <i.ewstafiew@yandex.ru>
2023-03-03 12:34:37 +0300

    added steps and annotations 'Allure'

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,3 +1,6 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,9 +10,11 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-
+@Owner("ewstafiew")
+@Feature("Выбор ветки и Поиск")
 public class SearchTest {
 
     @BeforeEach
@@ -18,48 +23,80 @@ public class SearchTest {
     }
 
 
+
     @Test
+    @Story("Смена ветки в GitHub")
     @DisplayName("Переключение на ветку fixtures")
     public void SwitchingToTheFixturesBranch() {
-        TestPages.junit4Page.branchSelectionButton()
-                .click();
-        TestPages.junit4Page.itemFixturesBranch()
-                .click();
-        TestPages.junit4Page.branchSelectionButton()
-                .shouldHave(text("fixtures"));
+
+        step("Кликнуть на кнопку выбора ветки", () -> {
+            TestPages.junit4Page.branchSelectionButton()
+                    .click();
+        });
+
+        step("Кликнуть на ветку fixtures", () -> {
+            TestPages.junit4Page.itemFixturesBranch()
+                    .click();
+        });
+
+        step("Открыта страница ветки fixtures", () -> {
+            TestPages.junit4Page.branchSelectionButton()
+                    .shouldHave(text("fixtures"));
+        });
     }
+
+
 
     @MethodSource("searchByNameAndNumbers")
     @ParameterizedTest(name = "{displayName} {0}")
+    @Story("Поиск по названию релиза на странице ветки fixtures")
     @DisplayName("Проверка поиска релизов")
     public void checkingTheReleaseSearch(String type, String searchValue) {
-        TestPages.junit4Page.branchSelectionButton()
-                .click();
-        TestPages.junit4Page.itemFixturesBranch()
-                .click();
-        TestPages.junit4Page.releasesButton()
-                .click();
-        TestPages.junit4Page.inputFindByRelease()
-                .sendKeys(searchValue);
-        TestPages.junit4Page.inputFindByRelease()
-                .pressEnter();
-        TestPages.junit4Page.releaseName()
-                .shouldHave(text(searchValue));
+
+        step("Кликнуть на кнопку выбора ветки", () -> {
+            TestPages.junit4Page.branchSelectionButton()
+                    .click();
+        });
+
+        step("Кликнуть на ветку fixtures", () -> {
+            TestPages.junit4Page.itemFixturesBranch()
+                    .click();
+        });
+
+        step("Кликнуть по ссылке 'Releases'", () -> {
+            TestPages.junit4Page.releasesButton()
+                    .click();
+        });
+
+        step("Ввести в инпут 'Find a release' поисковой запрос", () -> {
+            TestPages.junit4Page.inputFindByRelease()
+                    .sendKeys(searchValue);
+        });
+
+        step("Нажать 'enter' на клавиатуре", () -> {
+            TestPages.junit4Page.inputFindByRelease()
+                    .pressEnter();
+        });
+
+        step("В поисковой выдаче отображаются релизы с искомым значением в названии", () -> {
+            TestPages.junit4Page.releaseName()
+                    .shouldHave(text(searchValue));
+        });
     }
 
 
     static Stream<Arguments> searchByNameAndNumbers() {
         return Stream.of(
                 arguments(
-                        "Поиск по тексту в названии",
+                        "Поисковой запрос",
                         "JUnit"
                 ),
                 arguments(
-                        "Поиск по тексту в названии",
+                        "Поисковой запрос",
                         "Beta"
                 ),
                 arguments(
-                        "Поиск по цифрам в названии",
+                        "Поисковой запрос",
                         "4.12"
                 )
         );
